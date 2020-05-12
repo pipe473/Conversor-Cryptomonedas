3825062879a04675e6e2c973bf848400ad69d52f
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-12 18:51:10 +0200

    Definiendo bien la funcion del custom hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
-const useMoneda = () => ( 
+const useMoneda = () => {
+
+
+    const Seleccionar = () => (
     <Fragment>
         <label>Moneda</label>
         <select>
             <option value="MXN">Peso Mexicano</option>
         </select>
     </Fragment>
- );
+    );
+}
 
  
 export default useMoneda;
