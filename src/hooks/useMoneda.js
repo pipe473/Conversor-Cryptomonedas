e52f1d04622f87aaea694720b46ea052d37cecd0
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-12 19:37:46 +0200

    Añadiendo style components al select del custom hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -2,13 +2,23 @@ import React, { Fragment, useState } from 'react';
 import styled from '@emotion/styled';
 
 const Label = styled.label`
-    font-family: 'Beba Neue', cursive;
+    font-family: 'Bebas Neue', cursive;
     color: #FFF;
     text-transform: uppercase;
     font-weight: bold;
     font-size: 2.4rem;
     margin-top: 2rem;
     display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
 `
 
 const useMoneda = (label, stateInicial, opcionesMoneda) => {
@@ -19,8 +29,8 @@ const useMoneda = (label, stateInicial, opcionesMoneda) => {
 
     const Seleccionar = () => (
     <Fragment>
-        <label>{label}</label>
-        <select
+        <Label>{label}</Label>
+        <Select
             onChange={e => actualizarState(e.target.value)}
             value={state}
         >
@@ -28,7 +38,7 @@ const useMoneda = (label, stateInicial, opcionesMoneda) => {
             {opcionesMoneda.map(opcion =>(
                 <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
             ))}
-        </select>
+        </Select>
     </Fragment>
     );
 
