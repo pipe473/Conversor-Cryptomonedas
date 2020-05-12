5d783e09d0b620691ab5d7ca08fb1ec356f5ee2d
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-12 20:04:40 +0200

    Haciendo llamada a una API utilizando axios

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
 
 
 const Boton = styled.input`
@@ -38,7 +39,21 @@ const Formulario = () => {
     const [ moneda, SelectMonedas ] = useMoneda('Elige tu moneda', '', MONEDAS);
 
     // Utilizar useCriptomoneda
-    const [ criptomoneda, SelectCripto ] = useCriptomoneda('Elige tu criptomoneda', '')
+    const [ criptomoneda, SelectCripto ] = useCriptomoneda('Elige tu criptomoneda', '');
+
+    // Ejecutar llamado a la API
+    useEffect(() => {
+
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+        
+            const resultado = await axios.get(url);
+
+            console.log(resultado);
+            
+        }
+
+    }, []);
 
     return ( 
         <form>
