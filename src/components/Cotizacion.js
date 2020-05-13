9d8b40403759e25349631f6c523645d30f1797e1
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-14 00:01:23 +0200

    Añadiendo styled component a los resultados de los datos

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #FFF;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+    font-family: Arial, Helvetica, sans-serif;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+
+    span {
+        font-weight: bold;
+    }
+`;
 
 const Cotizacion = ({resultado}) => {
     if(Object.keys(resultado).length === 0) return null;
@@ -6,13 +28,13 @@ const Cotizacion = ({resultado}) => {
     console.log(resultado);
     
     return ( 
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>Precio más alto del día: <span>{resultado.HIGHDAY}</span></p>
-            <p>Precio más bajo del día: <span>{resultado.LOWDAY}</span></p>
-            <p>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></p>
-            <p>Última Actualización: <span>{resultado.LASTUPDATE}</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>Precio más alto del día: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>Precio más bajo del día: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></Info>
+            <Info>Última Actualización: <span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
      );
 }
  
