eadd550082ba93201ae46ae85e07712af763f973
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-13 23:49:30 +0200

    Mostrando el resultado an la UI

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -8,6 +8,10 @@ const Cotizacion = ({resultado}) => {
     return ( 
         <div>
             <p>El precio es: <span>{resultado.PRICE}</span></p>
+            <p>Precio más alto del día: <span>{resultado.HIGHDAY}</span></p>
+            <p>Precio más bajo del día: <span>{resultado.LOWDAY}</span></p>
+            <p>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></p>
+            <p>Última Actualización: <span>{resultado.LASTUPDATE}</span></p>
         </div>
      );
 }
